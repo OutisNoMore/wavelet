a5903df35238ce6c75eb1273370147b84b24cc0a
OutisNoMore <OutisNoMore@protonmail.com>
2022-10-14 18:30:39 -0700

    lab 3

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,59 +1,112 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
-    ArrayList<String> strings = new ArrayList<>(); // Array list of added strings
     // Error message
-    final String syntax = String.format("Syntax: /add?s=<string_to_add>\n" +
+    final String syntax = String.format("Syntax: /add?s=<string_to_add>&folder=<folder_to_put>&file=<file_to_put>\n" +
+                                        "\tValid folder types are: 1, 2, 3\n" +
+                                        "\tValid file types are: 1, 2\n" +
                                         "\t/search?s=<string_to_search>\n");
-    /*
-     * getStrings
-     *   Returns all strings in the list
-     * @return String
-     * @param void
-     */
-    private String getStrings(){
-      String output = "";
-      for(String s : strings){
-        if(s.equals(strings.get(strings.size()-1))){
-          output += "\"" + s + "\"\n";
-        }
-        else{
-          output += "\"" + s + "\", ";
+
+    private static void getFiles(File start, ArrayList<String> out, String target) {
+      if(!start.exists()){
+        System.out.println("File: " + start.getPath() + " does not exist");
+      }
+      if(start.isDirectory()){
+        File[] paths = start.listFiles();
+        for(File subFile: paths){
+          File test = new File(subFile.getPath());
+          getFiles(test, out, target);
         }
       }
-      return output;
+      else{
+        try(Scanner reader = new Scanner(start);){
+          while(reader.hasNextLine()){
+            if(reader.nextLine().contains(target)){
+              out.add(start.getPath());
+              break;
+            }
+          }
+        } catch(Exception e){
+          System.out.println("Error: " + e.getMessage());
+        } 
+      }
     }
 
     public String handleRequest(URI url) {
         String output = ""; // output string
         // Handle call to root
         if (url.getPath().equals("/")) {
-            output = syntax;
+            return syntax;
         } 
         // Handle call to add
         else if (url.getPath().contains("add")) {
-            String[] parameters = url.getQuery().split("=");  // get all queries
-            if(parameters[0].equals("s")){
-              strings.add(parameters[1]);
+            String[] parameters = url.getQuery().split("&");  // get all queries
+            String key = "";
+            String folder = "";
+            String file = "";
+            if(parameters.length != 3){
+              return syntax;
             }
-            output = String.format("Added: %s\nStrings are now: %s\n", parameters[1], this.getStrings());
+            if(parameters[0].charAt(0) == 's'){
+              key = parameters[0].substring(parameters[0].indexOf("=") + 1);
+            } else{
+              return syntax;
+            }
+            String call = parameters[1].substring(0, parameters[1].indexOf("="));
+            String param = parameters[1].substring(parameters[1].indexOf("=") + 1);
+            if(call.equals("folder")){
+              switch(param.charAt(0)){
+                case '1':
+                  folder = "Add1";
+                  break;
+                case '2':
+                  folder = "Add1/Add2";
+                  break;
+                case '3':
+                  folder = "Add1/Add2/Add3";
+                  break;
+                default:
+                  return syntax;
+              }
+            }
+            call = parameters[2].substring(0, parameters[2].indexOf("="));
+            param = parameters[2].substring(parameters[2].indexOf("=") + 1);
+            if(call.equals("file")){
+              if(param.charAt(0) < '1' || param.charAt(0) > '2'){
+                return syntax;
+              }
+              file = "file" + param;
+            } else{
+              return syntax;
+            }
+            File out = new File(folder + "/" + file);
+            try{
+              Files.write(out.toPath(), key.getBytes());
+            } catch(Exception e){
+              return String.format("Error: %s", e.getMessage());
+            }
+            output = String.format("Added: %s to %s\n\n", key, out.getPath());
         } 
         // Handle call to search 
         else if(url.getPath().contains("search")){
             String[] parameters = url.getQuery().split("=");  // get all queries
-            if(parameters[0].equals("s")){
-              output = String.format("Searching for %s:\nFound: ", parameters[1]);
-              for(String s : strings){
-                if(s.contains(parameters[1])){
+              if(parameters[0].equals("s")){
+                output = String.format("Searching for \"%s\":\n", parameters[1]);
+                ArrayList<String> results = new ArrayList<>();
+                getFiles(new File("."), results, parameters[1]);
+                output += String.format("Found in %d files:\n", results.size());
+                for(String s : results){
                   output += s + ", ";
                 }
+                output = output.substring(0, output.length() - 2);
               }
-              output = output.substring(0, output.length() - 2);
-            }
         }
         // Invalid url
         else{
